43da6d5f160a389b500c2c02a318909cd6c460ee
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2017-05-08 05:35:09 +0900

    align

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 )
 
 var (
-	ErrInvalidChain = errors.New("invalid chain")
-	ErrInvalidBlock = errors.New("invalid block")
-
 	apiAddr   = flag.String("api", ":3001", "api server address")
 	p2pAddr   = flag.String("p2p", ":6001", "p2p server address")
 	p2pOrigin = flag.String("origin", "ws://localhost", "p2p origin")
+
+	ErrInvalidChain = errors.New("invalid chain")
+	ErrInvalidBlock = errors.New("invalid block")
 )
 
 func main() {
